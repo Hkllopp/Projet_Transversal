01575ddfa423479b9362f91203e96c6045329245
fluffy-bastard <47632790+fluffy-bastard@users.noreply.github.com>
2019-12-19 17:18:59 +0100

    Update receive.py

--- IOT/Recepteur/receive.py
+++ IOT/Recepteur/receive.py
@@ -13,7 +13,7 @@ def parser(test_str):
     for matchNum, match in enumerate(matches, start=1):
     
         #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-        print("{match}")
+        print("{match}".format(match=match.group()))
         for groupNum in range(0, len(match.groups())):
             groupNum = groupNum + 1
         
